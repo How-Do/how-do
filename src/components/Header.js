a0e28b98d35bee33bbae71733a1b9ba6e6259515
MegSherman <megoftheshermans@gmail.com>
2020-07-10 15:53:06 -0600

    header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from './../images/raccoon-tail.png'
+// import { connect } from 'react-redux'
+// import { setUser } from '../../redux/reducer'
+// import { useHistory } from 'react-router-dom'
+// import { logout } from '../../../server/authController'
+
+const Header = () => {
+  return (
+    <div className='header-bar'>
+      <img src={logo} alt='logo' />
+      <input placeholder='How do I...' />
+      <a href='http://localhost:3001/auth'>
+        <button>Log In</button>
+      </a>
+    </div>
+  )
+}
+
+export default Header
