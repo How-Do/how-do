ef6e545a76cf270fedcbf7b7271f33dd9b3d08c6
AvocadoCasualty <baibeebambi@gmail.com>
2020-07-10 13:07:11 -0700

    Server outline set up.

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express'),
+    session = require('express-session'),
+    massive = require('massive');
+const {SESSION_SECRET, SERVER_PORT, CONNECTION_STRING} = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(session({
+        resave: false,
+        saveUninitialized: true,
+        cookie: {maxAge: 1000 * 60 * 60 * 24 * 14},
+        secret: SESSION_SECRET
+    })
+);
+app.use(express.static(`${__dirname}/..build`));
+
+
+//auth endpoints
+
+//app endpoints
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('The db is connected!')
+    app.listen(SERVER_PORT, () => console.log(`Server receiving trash deposits on port ${SERVER_PORT}`))
+}).catch(error => console.log(error))
