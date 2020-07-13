db0061f8afd0c170898d1cdcd37ebb98e8564bca
AvocadoCasualty <baibeebambi@gmail.com>
2020-07-13 09:16:16 -0700

    Dashboard skeleton

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,20 @@
 import React from "react";
+
+function Dashboard() {
+
+    return (
+        <div className='dashboard'>
+            <div className='categories'>
+                <div className='each-category'>
+
+                </div>
+
+            </div>
+            <div className='posts'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
