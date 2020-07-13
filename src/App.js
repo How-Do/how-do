73d7172848727bc9873bf756df66fde0ed53ef80
BrianDeSplinter <bdesplinter34@gmail.com>
2020-07-13 14:18:02 -0600

    Added temp access to Post component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import Post from './components/Post'
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      {/* ⬇This is just to see the post form, this will be a route from our new post button ➕*/}
+      <Post/>
     </div>
   )
 }
