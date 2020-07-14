1a08a3a6a46ad05c979aafbe09c178bcb1a26476
BrianDeSplinter <bdesplinter34@gmail.com>
2020-07-14 14:12:33 -0600

    Added Pic input

--- db/add_how_do.sql
+++ db/add_how_do.sql
@@ -1,4 +1,6 @@
 insert into posts
-(user_id, category, description, title)
+(user_id, title, description, post_pic, category)
 values
-($1, $2, $3, $4)
+($1, $2, $3, $4, $5);
+
+select * from posts;
