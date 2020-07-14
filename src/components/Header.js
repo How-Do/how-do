9ea7f17c20f41cc0f0a767e9c0e1684796bef28c
AvocadoCasualty <baibeebambi@gmail.com>
2020-07-14 11:03:54 -0700

    Fixed routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './../images/raccoon-tail.png'
 import { useAuth0 } from "@auth0/auth0-react";
+import {Link} from "react-router-dom";
 
 
 const Header = () => {
@@ -8,6 +9,13 @@ const Header = () => {
   return (
     <div className='header-bar'>
       <img src={logo} alt='logo' />
+      <div className='links'>
+          <Link to={'/'}> Dashboard </Link>
+          <Link to={'/addpost'}> Add Post </Link>
+          <Link to={'/post'}> Post </Link>
+          <Link to={'/profile'}> Profile </Link>
+          <Link to={'/favorites'}> Favorites </Link>
+      </div>
       <input placeholder='How do I...' />
       <button>Search</button>
         <button button onClick={() => loginWithRedirect()}>Log In</button>
