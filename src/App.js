20bdec1981215f5879330fb8bf503224323c9d66
AislinnRay <aislinn.ray.m@gmail.com>
2020-07-14 14:14:10 -0600

    resolving conflicts on App.js and Header.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Post from './components/Post'
-<<<<<<< HEAD
 import Dashboard from './components/Dashboard'
 import './styles/styles.css'
-=======
 import Dashboard from "./components/Dashboard";
 import routes from "./routes";
 
@@ -13,19 +11,15 @@ import routes from "./routes";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import Home from './components/comments/Home';
->>>>>>> master
 
 function App() {
   return (
     <div className='App'>
       <Header />
-<<<<<<< HEAD
       {/* ⬇This is just to see the post form, this will be a route from our new post button ➕*/}
       <Post />
       <Dashboard />
-=======
         {routes}
->>>>>>> master
     </div>
   )
 }
