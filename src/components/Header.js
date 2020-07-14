2cd47ff25ad5a1dde4be5f051417e3814fe7da32
MegSherman <megoftheshermans@gmail.com>
2020-07-14 13:51:52 -0600

    master fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,17 @@
 import React from 'react'
-<<<<<<< HEAD
 import logo from './../images/big-raccoon-tail.png'
 import './../styles/styles.css'
 // import { connect } from 'react-redux'
 // import { setUser } from '../../redux/reducer'
 // import { useHistory } from 'react-router-dom'
 // import { logout } from '../../../server/authController'
-=======
-import logo from './../images/raccoon-tail.png'
-import { useAuth0 } from "@auth0/auth0-react";
-import {Link} from "react-router-dom";
-
->>>>>>> master
+import { useAuth0 } from '@auth0/auth0-react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0()
   return (
     <div className='header-bar'>
-<<<<<<< HEAD
       <div className='logo-block'>
         <img src={logo} alt='logo' className='logo' />
         <h1>howDo</h1>
@@ -26,22 +20,19 @@ const Header = () => {
         <input placeholder='How do I...' className='master-input-box' />
         <button className='master-button'>Search</button>
       </div>
-      <a href='http://localhost:3001/auth'>
-        <button className='master-button'>Log In</button>
-      </a>
-=======
-      <img src={logo} alt='logo' />
       <div className='links'>
-          <Link to={'/'}> Dashboard </Link>
-          <Link to={'/addpost'}> Add Post </Link>
-          <Link to={'/post'}> Post </Link>
-          <Link to={'/profile'}> Profile </Link>
-          <Link to={'/favorites'}> Favorites </Link>
+        <Link to={'/'}> Dashboard </Link>
+        <Link to={'/addpost'}> Add Post </Link>
+        <Link to={'/post'}> Post </Link>
+        <Link to={'/profile'}> Profile </Link>
+        <Link to={'/favorites'}> Favorites </Link>
       </div>
-      <input placeholder='How do I...' />
-      <button>Search</button>
-        <button button onClick={() => loginWithRedirect()}>Log In</button>
->>>>>>> master
+      <button
+        button
+        onClick={() => loginWithRedirect()}
+        className='master-button'>
+        Log In
+      </button>
     </div>
   )
 }
