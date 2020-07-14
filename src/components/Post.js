e44fa8e9f4d420f0d75342795b8da7e1ca2b912c
BrianDeSplinter <bdesplinter34@gmail.com>
2020-07-14 16:50:38 -0600

    Added functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from "react";
 import AddComment from './AddComment';
+import axios from 'axios'
 
 //This component still needs to be updated to be able to pull the user from session once login is working
 
-function Post() {
+function Post(props) {
   const [userId, setUserId] = useState(1); //<-- This needs to be dynamic
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [category, setCategory] = useState("");
+  const [timestamp, setTimestamp] = useState("");
+
+  useEffect(() => {
+      console.log('Props:', props);
+      
+        axios.get(`/howdo/post/${props.match.params.id}`)
+        .then(res => {   
+            console.log(res.data)        
+            setUserId(res.data.user_id)
+            setTitle(res.data.title)
+            setDescription(res.data.description)
+            setImageUrl(res.data.post_pic)
+            setCategory(res.data.category)
+            setTimestamp(res.data.created_at)
+        })
+}, [])
 
   return (
     <div className="Post-Container" style={{backgroundColor: "lightblue"}}>
       <div>
-        <p>Category</p>
-        <p>Username of Poster</p>
-        <div>Timestamp</div>
+        <p>{category}</p>
+        <p>Username</p>
+        <div>{timestamp}</div>
       </div>
       <div>
-        <p>Title</p>
+        <p>{title}</p>
         <img />
-        <p>Description</p>
+        <p>{description}</p>
       </div>
       <div className="Comments-Container">
         <AddComment/>
