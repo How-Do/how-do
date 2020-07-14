622b24cfca05478a22b569576d1d22c3c70e048e
MegSherman <megoftheshermans@gmail.com>
2020-07-14 12:11:43 -0600

    header styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Post from './components/Post'
-import Dashboard from "./components/Dashboard";
+import Dashboard from './components/Dashboard'
+import './styles/styles.css'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       {/* ⬇This is just to see the post form, this will be a route from our new post button ➕*/}
-      <Post/>
+      <Post />
       <Dashboard />
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import logo from './../images/raccoon-tail.png'
+import logo from './../images/big-raccoon-tail.png'
+import './../styles/styles.css'
 // import { connect } from 'react-redux'
 // import { setUser } from '../../redux/reducer'
 // import { useHistory } from 'react-router-dom'
@@ -8,11 +9,16 @@ import logo from './../images/raccoon-tail.png'
 const Header = () => {
   return (
     <div className='header-bar'>
-      <img src={logo} alt='logo' />
-      <input placeholder='How do I...' />
-      <button>Search</button>
+      <div className='logo-block'>
+        <img src={logo} alt='logo' className='logo' />
+        <h1>howDo</h1>
+      </div>
+      <div className='search-block'>
+        <input placeholder='How do I...' className='master-input-box' />
+        <button className='master-button'>Search</button>
+      </div>
       <a href='http://localhost:3001/auth'>
-        <button>Log In</button>
+        <button className='master-button'>Log In</button>
       </a>
     </div>
   )
