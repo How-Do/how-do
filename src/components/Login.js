4c12a64d35b12d8d4db3a6b48563df10564979fc
MegSherman <megoftheshermans@gmail.com>
2020-07-21 12:22:11 -0600

    fixed master conflicts

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,32 +9,12 @@ function Login() {
   const [password, setPassword] = useState('')
   const dispatch = useDispatch()
 
-<<<<<<< HEAD
   function login() {
     axios
       .post('/api/login', { username, password })
       .then((res) => dispatch(setUser(res.data)))
       .catch((error) => console.log(error))
   }
-=======
-    return (
-        <div className="Login">
-            <input
-                name={username}
-                value={username}
-                placeholder='Username'
-                onChange={(e) => setUsername(e.target.value)}/>
-            <br/>
-            <input
-                name={password}
-                value={password}
-                placeholder='Password'
-                type='password'
-                onChange={(e)=> setPassword(e.target.value)}/>
-            <br/>
-            <button onClick={login}>Login</button>
-            <Link to={'/Register'} className='register-link'>Register</Link>
->>>>>>> master
 
   return (
     <div className='Login'>
--- src/components/Register.js
+++ src/components/Register.js
@@ -6,49 +6,29 @@ import axios from 'axios'
 import { useHistory } from 'react-router-dom'
 
 function Register() {
-<<<<<<< HEAD
   const [username, setUsername] = useState('')
+  const [profilePic, setProfilePic] = useState('')
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
-  const [profilePic, setProfilePic] = useState('')
   const dispatch = useDispatch()
   const history = useHistory()
 
   const register = (e) => {
     e.preventDefault()
     axios
-      .post('/api/register', { username, password, email })
+      .post('/api/register', { username, profilePic, password, email })
       .then((res) => {
         dispatch(setUser(res.data))
         history.push(`/profile`)
       })
       .catch((error) => console.log(error))
   }
-=======
-    const [username, setUsername] = useState('');
-    const [profilePic, setProfilePic] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const dispatch = useDispatch();
-    const history = useHistory();
-
-    const register = (e) => {
-        e.preventDefault()
-        axios.post('/api/register', {username, profilePic, password, email})
-            .then((res) => {
-                dispatch(setUser(res.data))
-                history.push(`/profile`)
-            })
-            .catch(error => console.log(error))
-    }
->>>>>>> master
 
   return (
     <div className='register'>
       <div className='form-box'>
         <h1>HowDoer Registration:</h1>
 
-<<<<<<< HEAD
         <p>
           Tell us who you are, <br /> and we'll tell you what to do.
         </p>
@@ -88,42 +68,6 @@ function Register() {
       </div>
     </div>
   )
-=======
-            <p>Please complete the fields below!</p>
-            <div>
-                <form onSubmit={register}>
-                    <p>Username: </p>
-                    <input placeholder='username'
-                           name='username'
-                           value={username}
-                           onChange={(e) => setUsername(e.target.value)}/>
-                    <br/>
-                    <p>Profile Picture: </p>
-                    <input placeholder='Profile Pic'
-                           name='profilePic'
-                           value={profilePic}
-                           onChange={(e) => setProfilePic(e.target.value)}/>
-                    <br/>
-                    <p>Password:</p>
-                    <input  type='password'
-                            placeholder='password'
-                            name='password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}/>
-                    <br/>
-                    <p>E-Mail:</p>
-                    <input placeholder='email'
-                           name='email'
-                           value={email}
-                           onChange={(e) => setEmail(e.target.value)}/>
-                    <br/>
-                    <button type='submit'>Register</button>
-                </form>
-            </div>
-
-        </div>
-    );
->>>>>>> master
 }
 
 export default Register
