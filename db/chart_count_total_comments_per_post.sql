35b877eb1af91e3e1f06f9d530c4653f6918b9af
AislinnRay <aislinn.ray.m@gmail.com>
2020-07-21 10:01:01 -0600

    updated bcryptjs and package.js files

--- db/chart_count_total_comments_per_post.sql
+++ db/chart_count_total_comments_per_post.sql
@@ -6,8 +6,8 @@
 -- -- FROM comments
 -- -- WHERE user_id = $1;
 
--- SELECT 
--- COUNT(c.comment_id)
--- FROM posts p
--- left join comments c on (p.post_id = c.post_id)
--- WHERE p.user_id = $1;
+SELECT 
+COUNT(c.comment_id)
+FROM posts p
+left join comments c on (p.post_id = c.post_id)
+WHERE p.user_id = $1;
