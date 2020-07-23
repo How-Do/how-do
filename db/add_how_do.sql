21a18d1e8cbc6af0aeccee89d5f70b37eb71c9f3
BrianDeSplinter <bdesplinter34@gmail.com>
2020-07-23 17:12:18 -0600

    corrected queries

--- db/add_how_do.sql
+++ db/add_how_do.sql
@@ -3,4 +3,5 @@ insert into posts
 values
 ($1, $2, $3, $4, $5);
 
-select * from posts;
+select (select count(*)::int from comments where post_id = p.post_id) as comment_count, u.username, p.post_id, p.user_id, p.category, p.description, p.created_at, p.title, p.post_pic from posts p
+left join users u on (p.user_id = u.id);
--- db/get_comments.sql
+++ db/get_comments.sql
@@ -2,11 +2,11 @@ select
 c.comment_id, 
 c.user_id, 
 c.created_at, 
-c.comment_pic, 
+c.comment_pic,
 c.comment, 
 u.username,
 p.post_id
-from posts p
-left join users u on (p.user_id = u.id)
-left join comments c on (p.post_id = c.post_id)
+from comments c
+join users u on (c.user_id = u.id)
+join posts p on (c.post_id = p.post_id)
 where p.post_id = $1;
