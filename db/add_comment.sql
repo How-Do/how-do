a6b9e6418c177e3abfebf73776abc54137b921ba
BrianDeSplinter <bdesplinter34@gmail.com>
2020-07-23 18:15:05 -0600

    Updated query to return more info

--- db/add_comment.sql
+++ db/add_comment.sql
@@ -2,5 +2,16 @@ insert into comments
 (user_id, post_id, comment, comment_pic)
 values
 ($1, $2, $3, $4);
-select * from comments
-where post_id = $2;
+
+select 
+c.comment_id, 
+c.user_id, 
+c.created_at, 
+c.comment_pic,
+c.comment, 
+u.username,
+p.post_id
+from comments c
+join users u on (c.user_id = u.id)
+join posts p on (c.post_id = p.post_id)
+where p.post_id = $2;
